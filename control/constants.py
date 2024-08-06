9c3ae8fba0e96d46984f3f0ab85c813b2c5664fe
morquecho-s <mssm011009@gmail.com>
2024-08-05 19:02:03 -0700

    update: se agrego el paquete de .vscode/ en el gitignore

--- control/constants.py
+++ control/constants.py
@@ -5,3 +5,11 @@ from enum import Enum,auto,unique
 class StackOptions(Enum):
     HORIZONTAL = auto()
     VERTICAL = auto()
+#* directions to paste
+HORIZONTAL = 17
+VERTICAL = 10
+# * scales
+CONTAIN = 11
+COVER = 12 # default in mark stack
+FIRST_SCALE_DOWN = 13
+INHERIT = 14
